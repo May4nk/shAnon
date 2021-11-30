bf8aa386bef45ffbc4f996d194ac5f8cfc8e6a15
May4nk <78518826+May4nk@users.noreply.github.com>
2021-12-01 01:56:54 +0530

    Update app.js

--- anon/static/app.js
+++ anon/static/app.js
@@ -12,21 +12,7 @@ function getcsrfToken(name){
 	}
 	return cookieVal;
 }
-/*
-(function($) {
-    $.fn.clickToggle = function(func1, func2) {
-        var funcs = [func1, func2];
-        this.data('toggleclicked', 0);
-        this.click(function() {
-            var data = $(this).data();
-            var tc = data.toggleclicked;
-            $.proxy(funcs[tc], this)();
-            data.toggleclicked = (tc + 1) % 2;
-        });
-        return this;
-    };
-}(jQuery));
-*/
+
 function pic(e,tag,input){
 	var reader = new FileReader();
 	reader.onload = function (){
